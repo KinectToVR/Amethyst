48ec24b0ddd7952258aa1fd5e2d344c027909a4c
公彦赤屋先 <kimihikoakaya@gmail.com>
2021-12-14 04:14:36 +0900

    Added device plugin: KinectV2 (K2App dynamic loaded tracking device)

---
+++ device_KinectV2/KinectV2Handler.cpp
@@ -0,0 +1,247 @@
+#include "pch.h"
+#include "KinectV2Handler.h"
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <Ole2.h>
+
+#include <ppl.h>
+#include <thread>
+#include <chrono>
+
+HRESULT KinectV2Handler::getStatusResult()
+{
+	BOOLEAN avail;
+	kinectSensor->get_IsAvailable(&avail);
+	if (avail)
+		return S_OK;
+	return S_FALSE;
+	// Hresult only actually determines whether the function worked, the bool is the true value....
+}
+
+std::string KinectV2Handler::statusResultString(HRESULT stat)
+{
+	switch (stat)
+	{
+	case S_OK: return "S_OK";
+	case S_FALSE: return "Sensor Unavailable! Check if it's plugged in to your USB and power plugs";
+	default: return "Uh Oh undefined kinect error! " + std::to_string(stat);
+	}
+}
+
+void KinectV2Handler::initialize()
+{
+	try
+	{
+		initialized = initKinect();
+		// initializeColor();
+		// Commented both image frames out, as most people use the kinect for skeletal data
+		// Updating all of the arrays uses a shit ton of CPU, but then again, it's still WIP
+		// initializeDepth();
+		initializeSkeleton();
+		if (!initialized) throw FailedKinectInitialization;
+	}
+	catch (std::exception& e)
+	{
+	}
+}
+
+void KinectV2Handler::initializeSkeleton()
+{
+	if (bodyFrameReader)
+		bodyFrameReader->Release();
+	IBodyFrameSource* bodyFrameSource;
+	kinectSensor->get_BodyFrameSource(&bodyFrameSource);
+	bodyFrameSource->OpenReader(&bodyFrameReader);
+
+	// Newfangled event based frame capture
+	// https://github.com/StevenHickson/PCL_Kinect2SDK/blob/master/src/Microsoft_grabber2.cpp
+	h_bodyFrameEvent = (WAITABLE_HANDLE)CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	HRESULT hr = bodyFrameReader->SubscribeFrameArrived(&h_bodyFrameEvent);
+	if (bodyFrameSource) bodyFrameSource->Release();
+}
+
+void KinectV2Handler::terminateSkeleton()
+{
+	if (bodyFrameReader)
+	{
+		HRESULT hr = bodyFrameReader->UnsubscribeFrameArrived(h_bodyFrameEvent);
+		if (FAILED(hr))
+		{
+			throw std::exception("Couldn't unsubscribe frame!");
+		}
+		CloseHandle((HANDLE)h_bodyFrameEvent);
+		h_bodyFrameEvent = NULL;
+
+		bodyFrameReader->Release();
+		bodyFrameReader = nullptr;
+	}
+}
+
+void KinectV2Handler::update()
+{
+	if (isInitialized())
+	{
+		BOOLEAN isAvailable = false;
+		HRESULT kinectStatus = kinectSensor->get_IsAvailable(&isAvailable);
+		if (kinectStatus == S_OK)
+		{
+			// NEW ARRIVED FRAMES ------------------------
+			MSG msg;
+			while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) // Unneccesary?
+			{
+				DispatchMessage(&msg);
+			}
+
+			if (h_bodyFrameEvent)
+			{
+				//printf("Kinect Event ID: %d\n" ,(int)h_bodyFrameEvent);
+
+				//now check for IR Events
+				HANDLE handles[] = {reinterpret_cast<HANDLE>(h_bodyFrameEvent)};
+				// , reinterpret_cast<HANDLE>(ke.hMSEvent)		};
+
+				switch (MsgWaitForMultipleObjects(_countof(handles), handles, false, 0, QS_ALLINPUT))
+				{
+				case WAIT_OBJECT_0:
+					{
+						IBodyFrameArrivedEventArgs* pArgs = nullptr;
+						//printf("Body Frame Event Signaled.\n");
+
+						if (bodyFrameReader)
+						{
+							HRESULT hr = bodyFrameReader->GetFrameArrivedEventData(h_bodyFrameEvent, &pArgs);
+							//printf("Retreive Frame Arrive Event Data -HR: %d\n", hr);
+
+							if (SUCCEEDED(hr))
+							{
+								//printf("Retreived Frame Arrived Event Data\n");
+								onBodyFrameArrived(*bodyFrameReader, *pArgs);
+								pArgs->Release();
+								//printf("Frame Arrived Event Data Released\n");
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+}
+
+void KinectV2Handler::onBodyFrameArrived(IBodyFrameReader& sender, IBodyFrameArrivedEventArgs& eventArgs)
+{
+	updateSkeletalData();
+}
+
+void KinectV2Handler::updateSkeletalData()
+{
+	if (bodyFrameReader)
+	{
+		IBodyFrame* bodyFrame = nullptr;
+		HRESULT frameReceived = bodyFrameReader->AcquireLatestFrame(&bodyFrame);
+		if (FAILED(frameReceived))
+		{
+			if (frameReceived == E_PENDING)
+			{
+				// LOG(INFO) << "Could not retrieve skeleton frame, stuck pending...";
+			}
+		}
+		//IBodyFrameReference* frameRef = nullptr;
+		//multiFrame->get_BodyFrameReference(&frameRef);
+		//frameRef->AcquireFrame(&bodyFrame);
+		//if (frameRef) frameRef->Release();
+		//if (bodyFrameReader) bodyFrameReader->Release();
+		if (!bodyFrame) return;
+
+		bodyFrame->GetAndRefreshBodyData(BODY_COUNT, kinectBodies);
+		newBodyFrameArrived = true;
+		if (bodyFrame) bodyFrame->Release();
+
+		// We have the frame, now parse it
+		updateParseFrame();
+	}
+}
+
+void KinectV2Handler::updateParseFrame()
+{
+	for (int i = 0; i < BODY_COUNT; i++)
+	{
+		if (kinectBodies[i])
+			kinectBodies[i]->get_IsTracked(&isTracking);
+		if (isTracking)
+		{
+			skeletonTracked = true;
+			kinectBodies[i]->GetJoints(JointType_Count, joints);
+			kinectBodies[i]->GetJointOrientations(JointType_Count, boneOrientations);
+
+			/* Copy joint positions */
+			for (int j = 0; j < ktvr::Joint_Total; ++j)
+			{
+				//TrackingDeviceBase::jointPositions[j].w = 1.0f;
+				TrackingDeviceBase::jointPositions[j].x() = joints[globalIndex[j]].Position.X;
+				TrackingDeviceBase::jointPositions[j].y() = joints[globalIndex[j]].Position.Y;
+				TrackingDeviceBase::jointPositions[j].z() = joints[globalIndex[j]].Position.Z;
+
+				TrackingDeviceBase::trackingStates[j] = joints[globalIndex[j]].TrackingState;
+			}
+
+			/* Copy joint orientations */
+			for (int k = 0; k < ktvr::Joint_Total; ++k)
+			{
+				TrackingDeviceBase::jointOrientations[k].w() = boneOrientations[globalIndex[k]].Orientation.w;
+				TrackingDeviceBase::jointOrientations[k].x() = boneOrientations[globalIndex[k]].Orientation.x;
+				TrackingDeviceBase::jointOrientations[k].y() = boneOrientations[globalIndex[k]].Orientation.y;
+				TrackingDeviceBase::jointOrientations[k].z() = boneOrientations[globalIndex[k]].Orientation.z;
+			}
+
+			newBodyFrameArrived = false;
+			break; // Only first skeleton
+		}
+		skeletonTracked = false;
+	}
+}
+
+bool KinectV2Handler::initKinect()
+{
+	if (FAILED(GetDefaultKinectSensor(&kinectSensor)))
+	{
+		return false;
+	}
+	if (kinectSensor)
+	{
+		kinectSensor->get_CoordinateMapper(&coordMapper);
+
+		HRESULT hr_open = kinectSensor->Open();
+		//kinectSensor->OpenMultiSourceFrameReader( FrameSourceTypes::FrameSourceTypes_Body| FrameSourceTypes::FrameSourceTypes_Depth
+		//    | FrameSourceTypes::FrameSourceTypes_Color,
+		//   &frameReader);
+		//return frameReader;
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		// Necessary to allow kinect to become available behind the scenes
+
+		BOOLEAN available = false;
+		kinectSensor->get_IsAvailable(&available);
+
+		if (FAILED(hr_open) || !available)
+		{
+			return false;
+		}
+		return true;
+	}
+	return false;
+}
+
+void KinectV2Handler::shutdown()
+{
+	try
+	{
+		// Release the Kinect sensor, called form k2vr
+		// OR from the crash handler
+		kinectSensor->Release();
+	}
+	catch (std::exception& e)
+	{
+	}
+}
---
+++ device_KinectV2/KinectV2Handler.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <Kinect.h>
+#include <Windows.h>
+
+#include "KinectToVR_API.h"
+
+/* Not exported */
+
+class KinectV2Handler : public ktvr::TrackingDeviceBase
+{
+public:
+	KinectV2Handler()
+	{
+		KinectV2Handler::initialize();
+		TrackingDeviceBase::deviceType = ktvr::K2_Kinect;
+	}
+
+	virtual ~KinectV2Handler()
+	{
+	}
+
+	IKinectSensor* kinectSensor = nullptr;
+	IBodyFrameReader* bodyFrameReader = nullptr;
+	IColorFrameReader* colorFrameReader = nullptr;
+	IDepthFrameReader* depthFrameReader = nullptr;
+	IMultiSourceFrame* multiFrame = nullptr;
+	ICoordinateMapper* coordMapper = nullptr;
+	BOOLEAN isTracking = false;
+
+	Joint joints[JointType_Count];
+	JointOrientation boneOrientations[JointType_Count];
+	IBody* kinectBodies[BODY_COUNT] = {nullptr};
+
+	HRESULT getStatusResult() override;
+	std::string statusResultString(HRESULT stat) override;
+
+	void initialize() override;
+	void update() override;
+	void shutdown() override;
+
+	bool convertColorToDepthResolution = false;
+	void onBodyFrameArrived(IBodyFrameReader& sender, IBodyFrameArrivedEventArgs& eventArgs);
+	virtual void updateSkeletalData();
+
+private:
+	bool initKinect();
+	void updateParseFrame();
+	void initializeSkeleton();
+	void terminateSkeleton();
+
+	WAITABLE_HANDLE h_bodyFrameEvent;
+	bool newBodyFrameArrived = false;
+
+	/* For translating Kinect joint enumeration to K2 space */
+	int globalIndex[25] = {3, 2, 20, 4, 5, 6, 7, 21, 22, 8, 9, 10, 11, 23, 24, 1, 0, 12, 13, 14, 15, 16, 17, 18, 19};
+};
+
+/* Exported for dynamic linking */
+
+extern "C" __declspec(dllexport) void* TrackingDeviceBaseFactory(const char* pVersionName, int* pReturnCode)
+{
+	static KinectV2Handler TrackingHandler; // Create a new device handler -> KinectV2
+	static ktvr::TrackingDeviceBase BaseTrackingHandler = // Slice the handler to the general handler
+		static_cast<ktvr::TrackingDeviceBase&>(TrackingHandler);
+
+	// Return the device handler for tracking
+	// but only if interfaces are the same / up-to-date
+	if (0 == strcmp(ktvr::IK2API_Version, pVersionName))
+	{
+		return &BaseTrackingHandler;
+	}
+
+	// Return code for initialization
+	(*pReturnCode) = ktvr::K2InitError_None;
+	if (pReturnCode)
+		*pReturnCode = ktvr::K2InitError_BadInterface;
+}
