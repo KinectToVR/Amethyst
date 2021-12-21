8b8ebba289505e4d98a56311e757d63a87efd857
公彦赤屋先 <kimihikoakaya@gmail.com>
2021-12-22 04:21:12 +0900

    Updated Settings Page for the new design

--- KinectToVR/SettingsPage.xaml.cpp
+++ KinectToVR/SettingsPage.xaml.cpp
@@ -10,130 +10,130 @@ using namespace Microsoft::UI::Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+// LMAO Eat Dirt Micro&Soft
+// Imma just cache object before the fancy UWP delegation reownership
+std::shared_ptr<Controls::Button> restartButton;
+
+// Helper local variables
+bool settings_localInitFinished = false;
+
 namespace winrt::KinectToVR::implementation
 {
-    SettingsPage::SettingsPage()
-    {
-        InitializeComponent();
-    }
+	SettingsPage::SettingsPage()
+	{
+		InitializeComponent();
+
+		// Cache needed UI elements
+		restartButton = std::make_shared<Controls::Button>(RestartButton());
+
+		// Notify of the setup end
+		settings_localInitFinished = true;
+	}
+}
+
+void trackersConfigChanged()
+{
+	// Don't react to pre-init signals
+	if (!settings_localInitFinished)return;
+
+	// Compare with saved settings and unlock the restart
+	restartButton.get()->IsEnabled(true);
+}
+
+void winrt::KinectToVR::implementation::SettingsPage::WaistOnToggle_Checked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+{
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::WaistOnToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::WaistOnToggle_Unchecked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    WaistOnToggleButton().Content(box_value(L"Turn Off"));
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::WaistOnToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::LeftFootOnToggle_Checked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    WaistOnToggleButton().Content(box_value(L"Turn On"));
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::WaistEnabledToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::LeftFootOnToggle_Unchecked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    WaistEnabledToggleButton().Content(box_value(L"Disable"));
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::WaistEnabledToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::RightFootOnToggle_Checked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    WaistEnabledToggleButton().Content(box_value(L"Enable"));
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::LeftFootOnToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::RightFootOnToggle_Unchecked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    LeftFootOnToggleButton().Content(box_value(L"Turn Off"));
+	trackersConfigChanged();
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::LeftFootOnToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::FlipCheckBox_Checked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    LeftFootOnToggleButton().Content(box_value(L"Turn On"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::LeftFootEnabledToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::FlipCheckBox_Unchecked(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    LeftFootEnabledToggleButton().Content(box_value(L"Disable"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::LeftFootEnabledToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::WaistRotationOptionBox_SelectionChanged(
+	winrt::Windows::Foundation::IInspectable const& sender,
+	winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-    LeftFootEnabledToggleButton().Content(box_value(L"Enable"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::RightFootOnToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::FeetRotationOptionBox_SelectionChanged(
+	winrt::Windows::Foundation::IInspectable const& sender,
+	winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-    RightFootOnToggleButton().Content(box_value(L"Turn Off"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::RightFootOnToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::PositionFilterOptionBox_SelectionChanged(
+	winrt::Windows::Foundation::IInspectable const& sender,
+	winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e)
 {
-    RightFootOnToggleButton().Content(box_value(L"Turn On"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::RightFootEnabledToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::WaistEnabledToggle_Toggled(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    RightFootEnabledToggleButton().Content(box_value(L"Disable"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::RightFootEnabledToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::LeftFootEnabledToggle_Toggled(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    RightFootEnabledToggleButton().Content(box_value(L"Enable"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::SoundsEnabledToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::RightFootEnabledToggle_Toggled(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    SoundsEnabledToggleButton().Content(box_value(L"Disable Sounds"));
 }
 
 
-void winrt::KinectToVR::implementation::SettingsPage::SoundsEnabledToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
+void winrt::KinectToVR::implementation::SettingsPage::RestartButton_Click(
+	winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
-    SoundsEnabledToggleButton().Content(box_value(L"Enable Sounds"));
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::TrackingFreezeToggleButton_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
-{
-    TrackingFreezeToggleButton().Content(box_value(L"Freeze Tracking"));
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::TrackingFreezeToggleButton_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
-{
-    TrackingFreezeToggleButton().Content(box_value(L"Resume Tracking"));
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::ResetConfigButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
-{
-    // Reset?
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::SoundsVolumeSlider_ValueChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::Primitives::RangeBaseValueChangedEventArgs const& e)
-{
-    // Capture?
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::AutoSpawnCheckBox_Checked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
-{
-
-}
-
-
-void winrt::KinectToVR::implementation::SettingsPage::AutoSpawnCheckBox_Unchecked(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
-{
-
 }
