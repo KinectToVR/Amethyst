04e392c803324013e18c8ed44726776ec1953942
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-01-02 21:02:34 +0900

    Added shared objects namespace file

---
+++ KinectToVR/K2Shared.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "pch.h"
+
+namespace k2app
+{
+	namespace shared
+	{
+		namespace general
+		{
+			// General Page
+			inline std::shared_ptr<winrt::Microsoft::UI::Xaml::Controls::TextBlock>
+				deviceNameLabel,
+				deviceStatusLabel,
+				errorWhatText,
+				trackingDeviceErrorLabel;
+
+			inline std::shared_ptr<winrt::Microsoft::UI::Xaml::Controls::Grid>
+				errorButtonsGrid,
+				errorWhatGrid;
+		}
+
+		namespace devices
+		{
+			inline std::shared_ptr<winrt::Microsoft::UI::Xaml::Controls::TextBlock>
+				deviceNameLabel,
+				deviceStatusLabel,
+				errorWhatText,
+				trackingDeviceErrorLabel;
+
+			inline std::shared_ptr<winrt::Microsoft::UI::Xaml::Controls::Grid>
+				errorButtonsGrid,
+				errorWhatGrid;
+
+			inline std::shared_ptr<winrt::Microsoft::UI::Xaml::Controls::ListView> devicesListView;
+			inline std::binary_semaphore smphSignalCurrentUpdate{ 0 };
+		}
+	}
+
+	namespace interfacing
+	{
+		// Current tracking device: 0 is the default
+		inline uint32_t trackingDeviceID = 0;
+	}
+}
