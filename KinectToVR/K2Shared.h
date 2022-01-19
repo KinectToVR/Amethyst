407db9172f68427342e639d8f1e8888804d536ed
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-01-20 05:03:01 +0900

    Added override and jointsbasis selected joints defs to shared namespace

--- KinectToVR/K2Shared.h
+++ KinectToVR/K2Shared.h
@@ -72,5 +72,22 @@ namespace k2app
 		// Current tracking device: 0 is the default
 		inline uint32_t trackingDeviceID = 0;
 		inline int32_t overrideDeviceID = -1;
+
+		// Joint tracking device selected joints: 0s are the defaults
+		// On the first time refresh the joints are assigned like W0 L1 R2
+		inline std::array<int32_t, 3> // W,L,R
+			selectedTrackedJointID = { 0, 0, 0 };
+
+		// Current override joints: W,L,R and 0 is the default for waist
+		// Override joints may overlap, -1 is for combo disabled and 'None' displayed
+		inline std::array<int32_t, 3>
+			positionOverrideJointID = {0, -1, -1},
+			rotationOverrideJointID = {0, -1, -1};
+
+		// Current override joints: W,L,R and true is the default for waist
+		inline std::array<bool, 3>
+			isPositionOverriddenJoint = { true, false, false },
+			isRotationOverriddenJoint = { true, false, false };
+
 	}
 }
