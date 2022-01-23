ddce0dedbbedad102a74a189a47bc1612477ae44
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-01-24 02:57:20 +0900

    Added kalman and lowpass filter headers

---
+++ KinectToVR/LowpassFilter.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+#include "pch.h"
+
+class LowPassFilter
+{
+public:
+
+	LowPassFilter() :
+		output(0),
+		ePow(0)
+	{
+	}
+
+	LowPassFilter(double iCutOffFrequency, double iDeltaTime) :
+		output(0),
+		ePow(1.0 - exp(-iDeltaTime * 2.0 * 3.14159265358979323846 * iCutOffFrequency))
+	{
+		if (iDeltaTime <= 0)
+		{
+			ePow = 0;
+		}
+		if (iCutOffFrequency <= 0)
+		{
+			ePow = 0;
+		}
+	}
+
+	double update(double input)
+	{
+		return output += (input - output) * ePow;
+	}
+
+	double update(double input, double deltaTime, double cutoffFrequency)
+	{
+		reconfigureFilter(deltaTime, cutoffFrequency); //Changes ePow accordingly.
+		return output += (input - output) * ePow;
+	}
+
+	void reconfigureFilter(double deltaTime, double cutoffFrequency)
+	{
+		if (deltaTime <= 0)
+		{
+			ePow = 0;
+		}
+		if (cutoffFrequency <= 0)
+		{
+			ePow = 0;
+		}
+		ePow = 1 - exp(-deltaTime * 2.0 * 3.14159265358979323846 * cutoffFrequency);
+	}
+
+private:
+	double output;
+	double ePow;
+};
