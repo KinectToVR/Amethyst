1a035e732d356301c9592119ffd0653b553ee60d
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-02-06 05:22:02 +0900

    updateOverrideDeviceUI, but it's actually implemented

--- KinectToVR/TrackingDevices.h
+++ KinectToVR/TrackingDevices.h
@@ -183,4 +183,79 @@ namespace TrackingDevices
 		// Save settings
 		k2app::K2Settings.saveSettings();
 	}
+
+	// Select proper tracking device in the UI
+	inline void updateOverrideDeviceUI(uint32_t const& index)
+	{
+		if (TrackingDevicesVector.size() < 1) return; // Just give up
+
+		// Don't show ANYTHING if we 'ven 't selected an override device
+		const bool _show = (k2app::K2Settings.overrideDeviceID >= 0) && (TrackingDevicesVector.size() >= 2);
+		bool status_ok = false; // Assume failure :/
+
+		std::string deviceName = "[UNKNOWN]"; // Dummy name
+		std::string device_status = "E_UKNOWN\nWhat's happened here?"; // Dummy status
+
+		if (_show)
+		{
+			// Get the current tracking device
+			auto& overrideDevice = TrackingDevicesVector.at(index);
+
+			if (overrideDevice.index() == 0)
+			{
+				// Kinect Basis
+				const auto device = std::get<ktvr::K2TrackingDeviceBase_KinectBasis*>(overrideDevice);
+
+				device_status = device->statusResultString(device->getStatusResult());
+				deviceName = device->getDeviceName();
+			}
+			else if (overrideDevice.index() == 1)
+			{
+				// Joints Basis
+				const auto device = std::get<ktvr::K2TrackingDeviceBase_JointsBasis*>(overrideDevice);
+
+				device_status = device->statusResultString(device->getStatusResult());
+				deviceName = device->getDeviceName();
+			}
+
+			status_ok = device_status.find("S_OK") != std::string::npos;
+		}
+
+		/* Update the device in general tab */
+
+		// Check with this one, should be the same for all anyway
+		if (::k2app::shared::general::overrideErrorWhatText.get() != nullptr)
+		{
+			// Update the status here
+			const bool base_status_ok = ::k2app::shared::general::errorWhatGrid.get()->Visibility() ==
+				winrt::Microsoft::UI::Xaml::Visibility::Collapsed;
+			using namespace winrt::Microsoft::UI::Xaml;
+
+			// Don't show device errors if we've got a server error or base error OR no o_device
+			if (k2app::interfacing::serverDriverFailure || !base_status_ok || !_show)status_ok = true;
+
+			// Don't show ANYTHING if we 'ven 't selected an override device
+			::k2app::shared::general::overrideDeviceNameLabel.get()->Visibility(
+				_show ? Visibility::Visible : Visibility::Collapsed);
+			::k2app::shared::general::overrideDeviceStatusLabel.get()->Visibility(
+				_show ? Visibility::Visible : Visibility::Collapsed);
+
+			::k2app::shared::general::overrideErrorWhatText.get()->Visibility(
+				status_ok ? Visibility::Collapsed : Visibility::Visible);
+			::k2app::shared::general::overrideErrorWhatGrid.get()->Visibility(
+				status_ok ? Visibility::Collapsed : Visibility::Visible);
+			::k2app::shared::general::overrideErrorButtonsGrid.get()->Visibility(
+				status_ok ? Visibility::Collapsed : Visibility::Visible);
+			::k2app::shared::general::overrideDeviceErrorLabel.get()->Visibility(
+				status_ok ? Visibility::Collapsed : Visibility::Visible);
+
+			// Split status and message by \n
+			::k2app::shared::general::overrideDeviceNameLabel.get()->Text(wstring_cast(deviceName));
+			::k2app::shared::general::overrideDeviceStatusLabel.get()->Text(
+				wstring_cast(split_status(device_status)[0]));
+			::k2app::shared::general::overrideDeviceErrorLabel.get()->Text(
+				wstring_cast(split_status(device_status)[1]));
+			::k2app::shared::general::overrideErrorWhatText.get()->Text(wstring_cast(split_status(device_status)[2]));
+		}
+	}
 }
