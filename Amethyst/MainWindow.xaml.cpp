515328850ac55f51adb6f4a76fc5e620d4f5e827
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-06-09 23:07:23 +0900

    Fixed crash handler argument passing

--- Amethyst/MainWindow.xaml.cpp
+++ Amethyst/MainWindow.xaml.cpp
@@ -516,7 +516,7 @@ namespace winrt::KinectToVR::implementation
 				              (boost::dll::program_location().parent_path() / "K2CrashHandler" / "K2CrashHandler.exe ")
 				              .string().c_str(),
 				              (std::to_string(GetCurrentProcessId()) +
-					              "\"" + k2app::interfacing::thisLogDestination + "\"").c_str(),
+					              " \"" + k2app::interfacing::thisLogDestination + ".log\"").c_str(),
 				              nullptr,
 				              SW_SHOWDEFAULT);
 			}).detach();
