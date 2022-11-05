f39ab8b446d9d8d23021077fefe39e5d256d0ecd
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-11-06 05:01:22 +0900

    Added static solution assets

--- Amethyst/MainWindow.xaml.cs
+++ Amethyst/MainWindow.xaml.cs
@@ -2,38 +2,124 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Amethyst
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+namespace Amethyst;
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+/// <summary>
+///     An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void InitializerTeachingTip_ActionButtonClick(Microsoft.UI.Xaml.Controls.TeachingTip sender, object args)
+    {
+
+    }
+
+    private void InitializerTeachingTip_CloseButtonClick(Microsoft.UI.Xaml.Controls.TeachingTip sender, object args)
+    {
+
+    }
+
+    private void ReloadTeachingTip_CloseButtonClick(Microsoft.UI.Xaml.Controls.TeachingTip sender, object args)
+    {
+
+    }
+
+    private void NavView_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void NavView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+    {
+
+    }
+
+    private void NavView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
+    {
+
+    }
+
+    private void UpdateButton_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+    {
+
+    }
+
+    private void UpdateButton_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HelpButton_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+    {
+
+    }
+
+    private void ContentFrame_NavigationFailed(object sender, Microsoft.UI.Xaml.Navigation.NavigationFailedEventArgs e)
+    {
+
+    }
+
+    private void ButtonFlyout_Opening(object sender, object e)
+    {
+
+    }
+
+    private void ButtonFlyout_Closing(Microsoft.UI.Xaml.Controls.Primitives.FlyoutBase sender, Microsoft.UI.Xaml.Controls.Primitives.FlyoutBaseClosingEventArgs args)
+    {
+
+    }
+
+    private void InstallLaterButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void InstallNowButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HelpFlyoutDocsButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HelpFlyoutDiscordButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HelpFlyoutDevButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void HelpFlyoutLicensesButton_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void LicensesFlyout_Opening(object sender, object e)
+    {
+
+    }
+
+    private void LicensesFlyout_Closed(object sender, object e)
+    {
+
     }
 }
