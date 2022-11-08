5844a69675489e45f8f4083269aefac5084991c2
公彦赤屋先 <kimihikoakaya@gmail.com>
2022-11-09 03:38:16 +0900

    Added K2AppTracker calibration/RPC compose methods

--- Amethyst/Classes/K2AppTracker.cs
+++ Amethyst/Classes/K2AppTracker.cs
@@ -9,6 +9,7 @@ public class K2AppTracker : INotifyPropertyChanged
 {
     // Is this tracker enabled?
     private bool _isActive;
+    private bool _overridePhysics;
 
     // Internal filters' data
     private Vector3 _kalmanPosition = new(0);
@@ -121,6 +122,16 @@ public class K2AppTracker : INotifyPropertyChanged
         }
     }
 
+    public bool OverridePhysics
+    {
+        get => _overridePhysics;
+        set
+        {
+            _overridePhysics = value;
+            OnPropertyChanged();
+        }
+    }
+
     // Internal position filters
     //private LowPassFilter[] _lowPassFilter = new LowPassFilter[3];
     //private KalmanFilter _kalmanFilter = new KalmanFilter();
@@ -132,10 +143,10 @@ public class K2AppTracker : INotifyPropertyChanged
     // and to select it, the filter number must be < 0
     public Vector3 GetFilteredPosition(JointPositionTrackingOption? filter = null)
     {
-        var computedFilter = 
+        var computedFilter =
             NoPositionFilteringRequested // If filtering is force-disabled
-            ? JointPositionTrackingOption.NoPositionTrackingFilter
-            : filter ?? _positionTrackingFilterOption;
+                ? JointPositionTrackingOption.NoPositionTrackingFilter
+                : filter ?? _positionTrackingFilterOption;
 
         return computedFilter switch
         {
@@ -178,7 +189,219 @@ public class K2AppTracker : INotifyPropertyChanged
     public Quaternion GetFullOrientation(RotationTrackingFilterOption? filter = null)
     {
         return GetFilteredOrientation(filter) * Quaternion.CreateFromYawPitchRoll(
-            OrientationOffset.X, OrientationOffset.Y, OrientationOffset.Z);
+            OrientationOffset.Y, OrientationOffset.X, OrientationOffset.Z);
+    }
+
+    // Get calibrated position, w/ offsets & filters
+    public Vector3 GetFullCalibratedPosition(Quaternion calibrationRotation,
+        Vector3 calibrationTranslation, Vector3? calibrationOrigin = null,
+        JointPositionTrackingOption? filter = null)
+    {
+        // Construct the calibrated pose
+        return Vector3.Transform(GetFilteredPosition(filter) -
+                calibrationOrigin ?? Vector3.Zero, calibrationRotation) +
+            calibrationTranslation + calibrationOrigin ?? Vector3.Zero +
+            PositionOffset; // Unwrap, rotate, transform, wrap, offset
+    }
+
+    // Get calibrated orientation, w/ offsets & filters
+    public Quaternion GetFullCalibratedOrientation(Quaternion calibrationRotation,
+        RotationTrackingFilterOption? filter = null)
+    {
+        // Construct the calibrated orientation
+        var rawOrientation = GetFilteredOrientation(filter);
+
+        // Construct the calibrated orientation in eigen
+        // Note: Apply calibration only in some cases
+        if (OrientationTrackingOption != JointRotationTrackingOption.DisableJointRotation &&
+            OrientationTrackingOption != JointRotationTrackingOption.FollowHmdRotation)
+            rawOrientation = calibrationRotation * rawOrientation;
+
+        // Return the calibrated orientation with offset
+        return Quaternion.CreateFromYawPitchRoll(OrientationOffset.Y,
+            OrientationOffset.X, OrientationOffset.Z) * rawOrientation;
+    }
+
+    // Get filtered data
+    // By default, the saved filter is selected,
+    // and to select it, the filter number must be < 0
+    // Additionally, this adds the offsets
+    // Offset will be added after translation
+    public Vector3 GetCalibratedVector(Vector3 positionVector,
+        Quaternion calibrationRotation, Vector3 calibrationTranslation,
+        Vector3? calibrationOrigin = null)
+    {
+        // Construct the calibrated pose
+        return Vector3.Transform(positionVector -
+                calibrationOrigin ?? Vector3.Zero, calibrationRotation) +
+            calibrationTranslation + calibrationOrigin ?? Vector3.Zero +
+            PositionOffset; // Unwrap, rotate, transform, wrap, offset
+    }
+
+    // Get tracker base
+    // This is for updating the server with
+    // exclusive filtered data from K2AppTracker
+    // By default, the saved filter is selected
+    // Offsets are added inside called methods
+    public K2TrackerBase GetTrackerBase(Quaternion calibrationRotation,
+        Vector3 calibrationTranslation, Vector3 calibrationOrigin,
+        JointPositionTrackingOption? posFilter = null,
+        RotationTrackingFilterOption? oriFilter = null)
+    {
+        // Check if matrices are empty
+        var notCalibrated = calibrationRotation.IsIdentity &&
+                            calibrationTranslation.Equals(Vector3.Zero) &&
+                            calibrationOrigin.Equals(Vector3.Zero);
+
+        // Construct the return type
+        var trackerBase = new K2TrackerBase()
+        {
+            Data = new K2TrackerData { IsActive = IsActive, Role = Role, Serial = Serial },
+            Tracker = Role
+        };
+
+        var fullOrientation = notCalibrated
+            ? GetFullOrientation(oriFilter)
+            : GetFullCalibratedOrientation(
+                calibrationRotation, oriFilter);
+
+        var fullPosition = notCalibrated
+            ? GetFullPosition(posFilter)
+            : GetFullCalibratedPosition(calibrationRotation,
+                calibrationTranslation, calibrationOrigin, posFilter);
+
+        trackerBase.Pose.Orientation = new K2Quaternion
+        {
+            W = fullOrientation.W, X = fullOrientation.X, Y = fullOrientation.Y, Z = fullOrientation.Z
+        };
+
+        trackerBase.Pose.Position = new K2Vector3
+        {
+            X = fullPosition.X, Y = fullPosition.Y, Z = fullPosition.Z
+        };
+
+        if (!OverridePhysics) return trackerBase;
+
+        var fullVelocity = notCalibrated
+            ? PoseVelocity
+            : GetCalibratedVector(
+                PoseVelocity, calibrationRotation,
+                calibrationTranslation, calibrationOrigin);
+
+        var fullAcceleration = notCalibrated
+            ? PoseAcceleration
+            : GetCalibratedVector(
+                PoseAcceleration, calibrationRotation,
+                calibrationTranslation, calibrationOrigin);
+
+        var fullAngularVelocity = notCalibrated
+            ? PoseAngularVelocity
+            : GetCalibratedVector(
+                PoseAngularVelocity, calibrationRotation,
+                calibrationTranslation, calibrationOrigin);
+
+        var fullAngularAcceleration = notCalibrated
+            ? PoseAngularAcceleration
+            : GetCalibratedVector(
+                PoseAngularAcceleration, calibrationRotation,
+                calibrationTranslation, calibrationOrigin);
+
+        trackerBase.Pose.Physics.Velocity = new K2Vector3
+        {
+            X = fullVelocity.X,
+            Y = fullVelocity.Y,
+            Z = fullVelocity.Z
+        };
+
+        trackerBase.Pose.Physics.Acceleration = new K2Vector3
+        {
+            X = fullAcceleration.X,
+            Y = fullAcceleration.Y,
+            Z = fullAcceleration.Z
+        };
+
+        trackerBase.Pose.Physics.AngularVelocity = new K2Vector3
+        {
+            X = fullAngularVelocity.X,
+            Y = fullAngularVelocity.Y,
+            Z = fullAngularVelocity.Z
+        };
+
+        trackerBase.Pose.Physics.AngularAcceleration = new K2Vector3
+        {
+            X = fullAngularAcceleration.X,
+            Y = fullAngularAcceleration.Y,
+            Z = fullAngularAcceleration.Z
+        };
+
+        return trackerBase;
+    }
+
+    // Get tracker base
+    // This is for updating the server with
+    // exclusive filtered data from K2AppTracker
+    // By default, the saved filter is selected
+    // Offsets are added inside called methods
+    public K2TrackerBase GetTrackerBase(
+        JointPositionTrackingOption? posFilter = null,
+        RotationTrackingFilterOption? oriFilter = null)
+    {
+        // Construct the return type
+        var trackerBase = new K2TrackerBase()
+        {
+            Data = new K2TrackerData { IsActive = IsActive, Role = Role, Serial = Serial },
+            Tracker = Role
+        };
+
+        var fullOrientation = GetFullOrientation(oriFilter);
+        var fullPosition = GetFullPosition(posFilter);
+
+        trackerBase.Pose.Orientation = new K2Quaternion
+        {
+            W = fullOrientation.W,
+            X = fullOrientation.X,
+            Y = fullOrientation.Y,
+            Z = fullOrientation.Z
+        };
+
+        trackerBase.Pose.Position = new K2Vector3
+        {
+            X = fullPosition.X,
+            Y = fullPosition.Y,
+            Z = fullPosition.Z
+        };
+
+        if (!OverridePhysics) return trackerBase;
+
+        trackerBase.Pose.Physics.Velocity = new K2Vector3
+        {
+            X = PoseVelocity.X,
+            Y = PoseVelocity.Y,
+            Z = PoseVelocity.Z
+        };
+
+        trackerBase.Pose.Physics.Acceleration = new K2Vector3
+        {
+            X = PoseAcceleration.X,
+            Y = PoseAcceleration.Y,
+            Z = PoseAcceleration.Z
+        };
+
+        trackerBase.Pose.Physics.AngularVelocity = new K2Vector3
+        {
+            X = PoseAngularVelocity.X,
+            Y = PoseAngularVelocity.Y,
+            Z = PoseAngularVelocity.Z
+        };
+
+        trackerBase.Pose.Physics.AngularAcceleration = new K2Vector3
+        {
+            X = PoseAngularAcceleration.X,
+            Y = PoseAngularAcceleration.Y,
+            Z = PoseAngularAcceleration.Z
+        };
+
+        return trackerBase;
     }
 
     public void InitializeFilters()
@@ -207,8 +430,8 @@ public class K2AppTracker : INotifyPropertyChanged
             JointVelocity = PoseVelocity,
             PreviousJointOrientation = PreviousOrientation,
             PreviousJointPosition = PreviousPosition,
-            TrackingState = IsActive 
-                ? TrackedJointState.StateTracked 
+            TrackingState = IsActive
+                ? TrackedJointState.StateTracked
                 : TrackedJointState.StateNotTracked
         };
     }
