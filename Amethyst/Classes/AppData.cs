87d14a9df6115e61af13808d86726eae837a034b
公彦赤屋先 <kimihikoakaya@gmail.com>
2023-02-22 00:17:51 +0900

    Version bump, AppData should use auto-properties

--- Amethyst/Classes/AppData.cs
+++ Amethyst/Classes/AppData.cs
@@ -4,11 +4,11 @@ public static class AppData
 {
     // Internal version number
     public static (string Display, string Internal)
-        VersionString = ("1.2.0.0", "1.2.230000.1");
+        VersionString { get; } = ("1.2.0.2", "1.2.230222.1");
 
     public const uint InternalVersion = 5; // Amethyst version
     public const uint ApiVersion = 0; // API version
 
     // Application settings
-    public static AppSettings Settings = new();
+    public static AppSettings Settings { get; set; } = new();
 }
